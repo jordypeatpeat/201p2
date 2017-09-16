04463596c6fd5a9534a5f553862331792df48294
jordypeatpeat <jordy2211221@gmail.com>
2017-09-16 20:07:05 +1200

    Header now fade gradient, fills on overlay section show; working profile for WCC; added 'type' in profiles

--- js/script.js
+++ js/script.js
@@ -5,14 +5,13 @@ $(document).ready(function(){
         $("#profile").hide(); // This hides said divs on site loading.
         $("#stats").hide();
         $("#upload").hide();
+        $("#profile2").hide();
 
 
-        $("#button_profile").click(function() { //This checks when the profile button is pressed, if there's a div to close
-
-            $("#profile").slideToggle(200);
+        $("#gotoPro").click(function() {
+            $("#profile2").slideToggle(200);
             $("#markerSide").hide();
 
-                            
 
             if ($("#stats").is(":visible")) {  // i.e. #show is a div that could be visible
                 $("#stats").slideToggle("slow");
@@ -21,6 +20,29 @@ $(document).ready(function(){
             if ($("#upload").is(":visible")) {
                 $("#upload").slideToggle("slow");
             }
+            
+            if ($("#profile").is(":visible")) {  // i.e. #show is a div that could be visible
+                $("#profile").slideToggle("slow");
+            }      
+        });
+
+        $("#button_profile").click(function() { //This checks when the profile button is pressed, if there's a div to close
+
+            $("#profile").slideToggle(200);
+            $("#markerSide").hide();
+
+
+            if ($("#stats").is(":visible")) {  // i.e. #show is a div that could be visible
+                $("#stats").slideToggle("slow");
+            }
+
+            if ($("#upload").is(":visible")) {
+                $("#upload").slideToggle("slow");
+            }
+
+            if ($("#profile2").is(":visible")) {  // i.e. #show is a div that could be visible
+                $("#profile2").slideToggle("slow");
+            }                 
         });
 
         $("#button_stats").click(function() { //This checks when the profile button is pressed, if there's a div to close
@@ -36,10 +58,14 @@ $(document).ready(function(){
             if ($("#upload").is(":visible")) {
                 $("#upload").slideToggle("slow");
             }
+
+            if ($("#profile2").is(":visible")) {  // i.e. #show is a div that could be visible
+                $("#profile2").slideToggle("slow");
+            }            
         });
 
 
-
+        // PROFILE 'STATS' BUTTON CLICKED, CLOSE PROFILE AND OPEN STATS
         $("#pbutton_stats").click(function() { //This checks when the profile stats button is pressed, if there's a div to close
             $("#profile").hide();
             $("#stats").slideToggle(200);
@@ -60,26 +86,11 @@ $(document).ready(function(){
             if ($("#stats").is(":visible")) {
                 $("#stats").slideToggle("slow");
             }
+
+            if ($("#profile2").is(":visible")) {  // i.e. #show is a div that could be visible
+                $("#profile2").slideToggle("slow");
+            }                 
         });
-
-
-        // HIDE OVERLAY SECTIONS WHEN USING MAP TOOLS
-        // $('#navDrawer').click(function() { //This checks when the profile button is pressed, if there's a div to close
-            
-        //     if ($("#profile").is(":visible")) {  // i.e. #show is a div that could be visible
-        //         $("#profile").hide();
-        //     }
-
-        //     if ($("#stats").is(":visible")) {
-        //         $("#stats").hide();
-        //     }
-
-        //     if ($("#upload").is(":visible")) {
-        //         $("#upload").hide();
-        //     }
-
-        // });
-
 
 });
 
@@ -90,7 +101,7 @@ $(document).on( "change", "input[name=options]", function() {
         $("#showStatic1").slideToggle(500);
         $("#showStatic2").slideToggle(500);
 
-} );
+});
 
 //CHANGE BACKGROUND-MAP IMAGE
 $(document).on( "change", "input[name=heat]", function() {
@@ -101,4 +112,69 @@ $(document).on( "change", "input[name=heat]", function() {
             $('.mapLayout').css({ 'background-image' : 'url(images/mapGrey.png)' });
         }
 
-} );
+});
+
+
+// CHANGE HEADER OPACITY WITH OVERLAY
+// https://stackoverflow.com/questions/11962962/overriding-important-with-css-or-jquery
+$('#button_profile').click(function(){
+        if ($("#profile").is(":visible")) {          
+            // replace css with '!important' and timer of 200 millis
+            setTimeout(function(){
+                $( '.mdl-layout__header' ).each(function () {
+                        this.style.setProperty( 'background-color', 'rgba(0,0,0,0)', 'important' );
+                });
+            }, 200);
+        }
+        else {
+            $( '.mdl-layout__header' ).each(function () {
+                this.style.setProperty( 'background-color', 'rgba(0,0,0,0.75)', 'important' );
+            });           
+        }
+});
+$('#button_stats').click(function(){
+        if ($("#stats").is(":visible")) {          
+            // replace css with '!important' and timer of 200 millis
+            setTimeout(function(){
+                $( '.mdl-layout__header' ).each(function () {
+                        this.style.setProperty( 'background-color', 'rgba(0,0,0,0)', 'important' );
+                });
+            }, 200);
+        }
+        else {
+            $( '.mdl-layout__header' ).each(function () {
+                this.style.setProperty( 'background-color', 'rgba(0,0,0,0.75)', 'important' );
+            });           
+        }
+});
+$('#button_upload').click(function(){
+        if ($("#upload").is(":visible")) {          
+            // replace css with '!important' and timer of 200 millis
+            setTimeout(function(){
+                $( '.mdl-layout__header' ).each(function () {
+                        this.style.setProperty( 'background-color', 'rgba(0,0,0,0)', 'important' );
+                });
+            }, 200);
+        }
+        else {
+            $( '.mdl-layout__header' ).each(function () {
+                this.style.setProperty( 'background-color', 'rgba(0,0,0,0.75)', 'important' );
+            });           
+        }
+});
+
+$('#gotoPro').click(function(){
+        if ($("#profile2").is(":visible")) {          
+            // replace css with '!important' and timer of 200 millis
+            setTimeout(function(){
+                $( '.mdl-layout__header' ).each(function () {
+                        this.style.setProperty( 'background-color', 'rgba(0,0,0,0)', 'important' );
+                });
+            }, 200);
+        }
+        else {
+            $( '.mdl-layout__header' ).each(function () {
+                this.style.setProperty( 'background-color', 'rgba(0,0,0,0.75)', 'important' );
+            });           
+        }
+});
